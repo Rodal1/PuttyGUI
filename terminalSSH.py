77c251a4b6b1003e813240f282347a74d48bc478
AEtorresola <adriel.torresola@upr.edu>
2023-02-24 16:59:57 -0400

    Now working to start SSH

--- terminalSSH.py
+++ terminalSSH.py
@@ -1,16 +1,24 @@
-import paramiko
+#import paramiko
+#
+#command = ""
+#
+#host = "rumad.uprm.edu"
+#username = ""
+#password = ""
+#
+#client = paramiko.client.SSHClient()
+#client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#client.connect(host, username=username, password=password)
+#
+#_stdin, _stdout,_stderr = client.exec_command("estudiante")
+#
+#print(_stdout.read().decode())
+#client.close()
 
-command = ""
+import subprocess
 
-host = "rumad.uprm.edu"
-username = ""
-password = ""
+ssh_command = f"ssh rumad.uprm.edu -l estudiante"
 
-client = paramiko.client.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=username, password=password)
+result = subprocess.run(ssh_command,shell=True, capture_output=True, text=True)
 
-_stdin, _stdout,_stderr = client.exec_command("estudiante")
-
-print(_stdout.read().decode())
-client.close()
+print(result.stderr)
