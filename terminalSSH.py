efcd56d62d974b3b57c3282a47652c9524fe6a3b
Rodal1 <robert.rodriguez11@upr.edu>
2023-02-24 17:14:42 -0400

    Added private ssh key and changed command to popen.

--- terminalSSH.py
+++ terminalSSH.py
@@ -1,24 +1,7 @@
-#import paramiko
-#
-#command = ""
-#
-#host = "rumad.uprm.edu"
-#username = ""
-#password = ""
-#
-#client = paramiko.client.SSHClient()
-#client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#client.connect(host, username=username, password=password)
-#
-#_stdin, _stdout,_stderr = client.exec_command("estudiante")
-#
-#print(_stdout.read().decode())
-#client.close()
-
 import subprocess
 
-ssh_command = f"ssh rumad.uprm.edu -l estudiante"
+ssh_command = f"ssh -oHostKeyAlgorithms=+ssh-rsa rumad.uprm.edu -l estudiante"
 
-result = subprocess.run(ssh_command,shell=True, capture_output=True, text=True)
+result = subprocess.Popen(ssh_command,shell=True, text=True)
 
 print(result.stderr)
