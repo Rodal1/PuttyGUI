a13687bfe970316c5bcb43ccdc92c5028f3cf91c
AEtorresola <adriel.torresola@upr.edu>
2023-02-24 19:48:05 -0400

    New File testing in paramiko

--- terminalSSH.py
+++ terminalSSH.py
@@ -2,6 +2,10 @@ import subprocess
 
 ssh_command = f"ssh -oHostKeyAlgorithms=+ssh-rsa rumad.uprm.edu -l estudiante"
 
-result = subprocess.Popen(ssh_command,shell=True, text=True)
+result = subprocess.Popen(ssh_command, shell=True, text=True)
 
-print(result.stderr)
+print(result.stdout)
+#
+#results = dir(result)
+#
+#print([result.j for j in [i for i in results]])
